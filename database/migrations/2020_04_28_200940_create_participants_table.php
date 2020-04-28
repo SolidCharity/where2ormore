22cd74d49687e52aa738da728f8f2dec75233a4c
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-04-28 22:30:43 +0200

    add first two tables

---
+++ database/migrations/2020_04_28_200940_create_participants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id');
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->string('name');
+            $table->integer('count_adults');
+            $table->integer('count_children');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
