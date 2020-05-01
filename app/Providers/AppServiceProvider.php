40a8d8be9d548c80f5de6ad23493d365042d48b6
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-05-01 06:23:36 +0200

    avoid issue with artican migrate on older MySQL installation: see https://laravel-news.com/laravel-5-4-key-too-long-error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // avoid issue with artican migrate on older MySQL installation: see https://laravel-news.com/laravel-5-4-key-too-long-error
+        Schema::defaultStringLength(191);
     }
 }
